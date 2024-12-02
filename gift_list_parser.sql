1a6aad7a44a7d289a03ff41f6c5c52e839018f60
Laura Reese <73046964+laurareese03@users.noreply.github.com>
2024-12-01 20:24:27 -0600

    Create gift_list_parser.sql
    day 1 solution

---
+++ gift_list_parser.sql
@@ -0,0 +1,13 @@
+--each child: primary and backup gift choices, color preferences, gift complexity, gift department
+ 
+select c.name,
+wl.wishes ->> 'first_choice' AS first_choice,
+wl.wishes ->> 'second_choice' as second_choice,
+wl.wishes -> 'colors' -> 0 as favorite_color,
+JSON_ARRAY_LENGTH(wl.wishes -> 'colors') as color_count,
+CASE tc.difficulty_to_make WHEN 1 THEN 'Simple Gift' WHEN 2 THEN 'Moderate Gift' ELSE 'Complex Gift' END as gift_complexity,
+CASE tc.category WHEN 'outdoor' THEN 'Outside Workshop' WHEN 'educational' THEN 'Learning Workshop' ELSE 'General Workshop' END as gift_complexity
+from children c
+join wish_lists wl on c.child_id = wl.child_id
+join toy_catalogue tc on (wl.wishes ->> 'first_choice') = tc.toy_name
+order by c.name asc;
